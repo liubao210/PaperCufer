6ddcaadc9107d51f750b41595cb3721a37f82384
刘保 <liubao@MacBook-Pro-4.local>
2022-01-18 11:28:17 +0800

    V0.1

--- R/download_paper.R
+++ R/download_paper.R
@@ -1,7 +1,7 @@
 
 download_paper = function(keyword = 'REITs', cookies = 'DBA24B22726FA8ED9BE1A1353F601FAD', sleep_time = 0.5){
   query_results = get_query_results(keyword = keyword, cookie = cookies, sleep_time = sleep_time)
-  write.csv(query_results, "./query_result.csv")
+  # write.csv(query_results, "./query_result.csv")
 
   query_results %>%
     dplyr::select(paper_piclink, paper_name = col_2, paper_index = col_1) %>%
